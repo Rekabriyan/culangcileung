9193b9b404cfc34032fedc9982ad07ffb952bada
Rekabriyan <rekabriyanch08@gmail.com>
2023-12-09 15:52:43 +0700

    fix - about

--- app.py
+++ app.py
@@ -110,6 +110,11 @@ def deteksi_pose():
 def home():
     return render_template("index.html")
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 @app.route("/demo")
 def demo():
     return render_template("demo.html")
